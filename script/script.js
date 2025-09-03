0a14e2bdb212bd16b3ddcfce2351c7e64e43b933
AL-AMIN <alaminkazi4510@gmail.com>
2025-09-04 03:50:31 +0600

    payoo login page

---
+++ script/script.js
@@ -0,0 +1,31 @@
+//console.log('connected');
+
+//none of borderLine
+document.getElementById('pin_number').style.outline='none';
+document.getElementById('mobile_number').style.outline='none';
+
+//connected login_Form
+
+document.getElementById('login_btn')
+    .addEventListener('click',function(event){
+        event.preventDefault();
+        ///console.log("connected");
+
+
+        const PhoneNumber=document.getElementById('mobile_number').value;
+        const PinNumber=document.getElementById('pin_number').value;
+        //console.log(typeof(PhoneNumber),typeof(PinNumber));
+        //convert_phoneNum=parseInt(PhoneNumber);
+        convert_PinNum=parseInt(PinNumber);
+        //console.log(typeof(convert_phoneNum),typeof(convert_PinNum));
+
+        //const phone_number&& pin_number
+        const phone='01728481692';
+        const pin=123;
+        if(phone==PhoneNumber&&convert_PinNum===pin){
+            window.location.href="./home.html"
+        }
+        else{
+            alert("Please Enter Your Correct Credential");
+        }
+    });
